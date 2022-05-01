95bbf69e740fa4bd59c6a3e9c327b29d8d0ad2ec
juliswer <julianswer36@gmail.com>
2022-05-01 12:43:58 -0300

    register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,48 @@
+import { useState } from "react";
+
 export function Register() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user)
+    setUser({
+      email: "",
+      password: "",
+    })
+  }
+
   return (
-    <div>Register</div>
-  )
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        name="email"
+        id=""
+        placeholder="youremail@company.ltd"
+        value={user.email}
+        onChange={handleChange}
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        name="password"
+        id="password"
+        value={user.password}
+        onChange={handleChange}
+      />
+
+      <button>Register</button>
+    </form>
+  );
 }
